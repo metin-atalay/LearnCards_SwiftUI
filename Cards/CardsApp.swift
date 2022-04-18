49b408c6f491df6b72083bfb2b9d38e4cffef488
Metin ATALAY <metin.atalay@architecht.com>
2022-04-18 21:52:32 +0300

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Metin Atalay on 18.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
